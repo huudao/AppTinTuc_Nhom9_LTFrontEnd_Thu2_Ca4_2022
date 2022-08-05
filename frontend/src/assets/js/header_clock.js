6173402695aa6637e58c20f87d5f8f32e6f44d5c
skyversion <19130225@st.hcmuaf.edu.vn>
2022-08-05 18:24:49 +0700

    upcomoponment

---
+++ frontend/src/assets/js/header_clock.js
@@ -0,0 +1,11 @@
+
+var d = new Date();
+var ngay = d.getDate();
+var thang = d.getMonth()+1;
+var nam = d.getFullYear();
+
+document.getElementById("year").innerHTML=ngay+"/" +thang +"/"+ nam
+   
+
+ 
+   
